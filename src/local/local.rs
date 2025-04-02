f2cc69fa094618709dc0b0807bd638aca15eaaf2
Shahan Neda <shahan.neda@gmail.com>
2025-04-01 23:35:33 -0400

    added afterparty

--- src/local/local.rs
+++ src/local/local.rs
@@ -30,7 +30,8 @@ async fn main() {
     // let scene_name = "ninja/pomegranate_simplified";
     // let scene_name = "ninja/apple_voxel";
     // let scene_name = "ninja/apple_extra_full";
-    let scene_name = "neo_splat";
+    // let scene_name = "neo_splat";
+    let scene_name = "afterparty";
 
     // let scene_name = "ninja/apple_rotate";
     // let scene_name = "sci_01_edited";
